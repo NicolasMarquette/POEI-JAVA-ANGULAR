033e0cfa5bd78bd32e791fb4f895a831cad80c97
Nicolas Marquette <nicolas.marquette@yahoo.fr>
2022-05-04 17:13:19 +0200

    Update de la classe écrivant dans le fichier log rajout d'un espace entre id et datetime

--- projet-hopital/src/model/RapportFileAttente.java
+++ projet-hopital/src/model/RapportFileAttente.java
@@ -14,10 +14,9 @@ public class RapportFileAttente {
 		String filePath = "fileAttenteLog.txt";
 		FileWriter fw = new FileWriter(filePath, true);
 		BufferedWriter bw = new BufferedWriter(fw);
-		String lineToAppend = String.format("%s %s\n", id, dateTime);
+		String lineToAppend = String.format("%s  %s\n", id, dateTime);
 		bw.write(lineToAppend);
 		bw.close();
-
 	}
 
 }
