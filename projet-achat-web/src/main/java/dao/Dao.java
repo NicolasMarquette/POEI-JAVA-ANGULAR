58bbe0247dd4280c228a9ab749e13eed9eaf45d6
Nicolas Marquette <nicolas.marquette@yahoo.fr>
2022-05-16 17:05:35 +0200

    feat(dao): ajoute l'interface dao générique
    what:
    Ajoute une interface dao générique avec les méthodes CRUD :
    - findAll()
    - findById(id)
    - create(obj)
    - update(obj)
    - delete(obj)
    
    why:
    Permets de créer les interfaces spécifiques pour les tables dans la base sql 
    Puis les classes qui en découle.

---
+++ projet-achat-web/src/main/java/dao/Dao.java
@@ -0,0 +1,15 @@
+package dao;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface Dao<T, PK> {
+	
+	public List<T> findAll() throws ClassNotFoundException, SQLException, IOException;
+	public T findById(PK id) throws ClassNotFoundException, SQLException, IOException;
+	public void create(T obj) throws ClassNotFoundException, SQLException, IOException;
+	public void update(T obj) throws ClassNotFoundException, SQLException, IOException;
+	public void delete(T obj) throws ClassNotFoundException, SQLException, IOException;
+
+}
