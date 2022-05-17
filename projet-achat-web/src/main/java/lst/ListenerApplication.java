7bb8f1f37ae3431698fa173ecb52b72c94dbb9c7
Nicolas Marquette <nicolas.marquette@yahoo.fr>
2022-05-17 14:48:36 +0200

    feat(lst): ajoute listener chargeant table articles
    what:
    Implémente la méthode contextInitialized pour charger les données de la 
    table articles en mémoire sur l'application.
    
    why:
    Permets d'éviter de soliciter la table à chaque fois comme les articles ne 
    changent pas.

---
+++ projet-achat-web/src/main/java/lst/ListenerApplication.java
@@ -0,0 +1,56 @@
+package lst;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import dao.DaoArticleMySql;
+import model.Article;
+
+/**
+ * Application Lifecycle Listener implementation class ListenerApplication
+ *
+ */
+@WebListener
+public class ListenerApplication implements ServletContextListener {
+
+	/**
+	 * Default constructor.
+	 */
+	public ListenerApplication() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+	 */
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
+	 */
+	public void contextInitialized(ServletContextEvent sce) {
+
+		try {
+			ArrayList<Article> listeArticles = (ArrayList<Article>) new DaoArticleMySql().findAll();
+
+			sce.getServletContext().setAttribute("articles", listeArticles);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
